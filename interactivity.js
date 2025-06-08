c8c3909540be120e68daa817acdafcb94d12719b
kamilazabek <kamilazabek@gmail.com>
2025-06-09 01:43:26 +0200

    reverted greeting

--- interactivity.js
+++ interactivity.js
@@ -3,7 +3,7 @@ let input = document.querySelector(".input");
 const button = document.querySelector(".greet");
 button.addEventListener("click", function () {
   if (input.value != '') {
-    alert(`Hi ${input.value}! I'm glad you're visiting my page :D`);
+    alert(`Hello ${input.value}! I'm glad you're visiting my page :D`);
   }
 });
 
